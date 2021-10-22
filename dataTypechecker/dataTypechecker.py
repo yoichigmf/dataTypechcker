2e1a33d6f5fc885f63582331bd76f2c8825ca253
yoichi_gmf <yoichi.kayama@gmail.com>
2021-10-22 13:46:26 +0900

    Python3 対応

--- dataTypechecker/dataTypechecker.py
+++ dataTypechecker/dataTypechecker.py
@@ -5,14 +5,30 @@
 #
 import sys
 import os
+import unicodedata
 from osgeo import ogr
+
+def toUnicode(encodedStr):
+    '''
+    :return: an unicode-str.
+    '''
+    if isinstance(encodedStr, str):
+        return encodedStr
+
+    for charset in [u'cp932', u'utf-8', u'euc-jp', u'shift-jis', u'iso2022-jp']:
+        try:
+            return encodedStr.decode(charset)
+        except:
+            pass
 
 
 def   checkShapeFile( inputfile  , outputfile, resultfile):
 
     #daShapefile = r"G:\work\NHK_kumamoto\210909HDD\kumamoto\430005kumamotoken\その1\1 筑後川\L1\MAXALL.SHP"
     #daShapefile = r"C:\Temp\Voting_Centers_and_Ballot_Sites.shp"
-    daShapefile = unicode(inputfile, 'cp932') 
+    #daShapefile = unicode(inputfile, 'cp932') 
+    
+    daShapefile = toUnicode(inputfile) 
     driver = ogr.GetDriverByName('ESRI Shapefile')
 
     dataSource = driver.Open(daShapefile, 0) # 0 means read-only. 1 means writeable.
@@ -20,11 +36,12 @@ def   checkShapeFile( inputfile  , outputfile, resultfile):
     ofile = sys.stdout
    
 
+
 # Check to see if shapefile is found.
     if dataSource is None:
-        print 'Could not open %s' % (daShapefile)
+        print( 'Could not open ' )
     else:
-        print 'Opened %s' % (daShapefile)
+        print ('Opened %s' % (daShapefile))
         layer = dataSource.GetLayer()
         featureCount = layer.GetFeatureCount()
         #print "Number of features in %s: %d" % (os.path.basename(daShapefile),featureCount)
@@ -44,24 +61,24 @@ def   checkShapeFile( inputfile  , outputfile, resultfile):
                     tgdata = feature. GetField(keyname)
             #print( feature. GetField(keyname))
 
-                    if isinstance(tgdata, long):
+                    if isinstance(tgdata, int):
                         tglength = len(str(tgdata))
                           
                         if tglength != 13 and  tglength != 15 :
                             tglength = -1 
 
-                        output_str = str(tglength)+ "," + unicode(inputfile, 'cp932') + "\n"
+                        output_str = str(tglength)+ "," + toUnicode(inputfile) + "\n"
 
                         if outputfile is not None:
-                            with open(unicode(outputfile, 'cp932'), "a") as of:
+                            with open(toUnicode(outputfile), "a") as of:
                                 of.write(output_str)
                         else:
                             print(output_str)
 
-                        result_str =  str(tglength)+ "," + unicode(inputfile, 'cp932') +  "," + keyname + ","+ str(tgdata) + "\n"
+                        result_str =  str(tglength)+ "," + toUnicode(inputfile) +  "," + keyname + ","+ str(tgdata) + "\n"
 
                         if resultfile is not None:
-                            with open(unicode(resultfile, 'cp932'), "a") as rf:
+                            with open(toUnicode(resultfile), "a") as rf:
                                 rf.write(result_str )
                         else:
                             print(result_str)
@@ -73,18 +90,18 @@ def   checkShapeFile( inputfile  , outputfile, resultfile):
                         if tglength != 13 and  tglength != 15 :
                             tglength = -1 
 
-                        output_str = str(tglength)+ "," + unicode(inputfile, 'cp932') + "\n"
+                        output_str = str(tglength)+ "," + toUnicode(inputfile) + "\n"
 
                         if outputfile is not None:
-                            with open(unicode(outputfile, 'cp932'), "a") as of:
+                            with open(toUnicode(outputfile), "a") as of:
                                 of.write(output_str)
                         else:
                             print(output_str)
 
-                        result_str =  str(tglength)+ "," + unicode(inputfile, 'cp932') +  "," + keyname + ","+ str(tgdata) + ",string \n"
+                        result_str =  str(tglength)+ "," + toUnicode(inputfile) +  "," + keyname + ","+ str(tgdata) + ",string \n"
 
                         if resultfile is not None:
-                            with open(unicode(resultfile, 'cp932'), "a") as rf:
+                            with open(toUnicode(resultfile), "a") as rf:
                                 rf.write(result_str )
                         else:
                             print(result_str)
@@ -92,18 +109,18 @@ def   checkShapeFile( inputfile  , outputfile, resultfile):
                         break
                     else:
                         tglength  = -1
-                        output_str = str(tglength)+ "," + unicode(inputfile, 'cp932') + "\n"
+                        output_str = str(tglength)+ "," + toUnicode(inputfile) + "\n"
 
                         if outputfile is not None:
-                            with open(unicode(outputfile, 'cp932'), "a") as of:
+                            with open(toUnicode(outputfile), "a") as of:
                                 of.write(output_str)
                         else:
                             print(output_str)
 
-                        result_str =  str(tglength)+ "," + unicode(inputfile, 'cp932') +  "," + keyname + ","+ str(tgdata) + ",type unknown \n"
+                        result_str =  str(tglength)+ "," + toUnicode(inputfile) +  "," + keyname + ","+ str(tgdata) + ",type unknown \n"
 
                         if resultfile is not None:
-                            with open(unicode(resultfile, 'cp932'), "a") as rf:
+                            with open(toUnicode(resultfile), "a") as rf:
                                 rf.write(result_str )
                         else:
                             print(result_str)
@@ -111,18 +128,18 @@ def   checkShapeFile( inputfile  , outputfile, resultfile):
                         break
         else:
             tglength  = -1
-            output_str = str(tglength)+ "," + unicode(inputfile, 'cp932') + "\n"
+            output_str = str(tglength)+ "," + toUnicode(inputfile) + "\n"
 
             if outputfile is not None:
-                with open(unicode(outputfile, 'cp932'), "a") as of:
+                with open(toUnicode(outputfile), "a") as of:
                      of.write(output_str)
             else:
                 print(output_str)
 
-            result_str =  str(tglength)+ "," + unicode(inputfile, 'cp932') +  "," + keyname + ","+ str(tgdata) + ",no field \n"
+            result_str =  str(tglength)+ "," +toUnicode(inputfile) +  "," + keyname + ","+ str(tgdata) + ",no field \n"
 
             if resultfile is not None:
-                with open(unicode(resultfile, 'cp932'), "a") as rf:
+                with open(toUnicode(resultfile), "a") as rf:
                     rf.write(result_str )
             else:
                 print(result_str)
