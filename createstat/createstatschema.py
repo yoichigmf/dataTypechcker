3f0674679de72b05273cead3bdb1f0cae560279f
yoichi_gmf <yoichi.kayama@gmail.com>
2021-12-26 16:32:18 +0900

    add sum python module

---
+++ createstat/createstatschema.py
@@ -0,0 +1,8 @@
+# coding: UTF-8
+import argparse
+
+ARGSCHEME = argparse.ArgumentParser( description='check geometry')
+
+ARGSCHEME.add_argument('-i', '--inputfile', help='input file name')
+
+
---
+++ makemeshprm/testgeomschema.py
@@ -0,0 +1,8 @@
+# coding: UTF-8
+import argparse
+
+ARGSCHEME = argparse.ArgumentParser( description='make mesh parameter from mesh vector')
+
+ARGSCHEME.add_argument('-i', '--inputfile', help='input file name')
+
+
